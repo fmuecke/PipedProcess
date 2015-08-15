e3f40c151990164c0647e0f7eb77eac4efc4c6f8
Florian Mücke <fmuecke@gmail.com>
2015-08-15 14:44:53 +0200

    restructured DemoChildProc and added separate project file

--- DemoChildProc/DemoChildProc.cpp
+++ DemoChildProc/DemoChildProc.cpp
@@ -1,49 +1,78 @@
 #include <vector>
-#include <string>
 #include <io.h>
 #include <fcntl.h>
 #include <iostream>
 #include <fstream>
+#include <Windows.h>
 
 using namespace std;
 
 static char const * const fileName = "cin.out";
 
-int main()
+static int ReadInput(vector<char>& resultData)
 {
 	if (cin.good())
 	{
 		if (_setmode(_fileno(stdin), _O_BINARY) == -1)
 		{
 			cerr << "error switching stdin to binary mode: " << strerror(errno);
-			return -1;
-		}
-		cin.seekg(0, cin.end);
-		auto len = cin.tellg();
-		cin.seekg(0, cin.beg);
-		if (len > 0)
-		{
-			vector<char> v(static_cast<size_t>(len), 0);
-			cin.read(&v[0], len);
-			ofstream outfile(fileName, ios::binary | ios::trunc);
-			outfile.write(&v[0], v.size());
-			outfile.close();
-		
-			_setmode(_fileno(stdout), _O_BINARY);
-			cout.write(&v[0], v.size());
-			cout << len << " bytes written to '" << fileName << "'" << endl;
-			return 0;
 		}
 		else
 		{
-			cerr << "error: stdin contains no data" << endl;
+			cin.seekg(0, cin.end);
+			auto len = cin.tellg();
+			cin.seekg(0, cin.beg);
+			if (len > 0)
+			{
+				vector<char> v(static_cast<size_t>(len), 0);
+				cin.read(&v[0], len);
+				resultData.swap(v);
+				return 0;
+			}
 		}
-	}
-	else
-	{
-		cerr << "error: unable to open stdin" << endl;
+		cerr << "error: stdin contains no data" << endl;
 	}
 
+	cerr << "error: unable to open stdin" << endl;
 	return -1;
+}
+
+static int WriteOurput(vector<char> const& resultData)
+{
+	ofstream outfile(fileName, ios::binary | ios::trunc);
+	if (!outfile.good())
+	{
+		cerr << "error: cannot open output file '" << fileName << "' for writing" << endl;
+		return -1;
+	}
+	outfile.write(&resultData[0], resultData.size());
+	outfile.close();
+
+	if (_setmode(_fileno(stdout), _O_BINARY) == -1)
+	{
+		cerr << "error switching stdout to binary mode: " << strerror(errno);
+		return -1;
+	}
+	cout.write(&resultData[0], resultData.size());
+	cerr << resultData.size() << " bytes written to '" << fileName << "'" << endl;
+	
+	return 0;
+}
+
+int main()
+{
+	vector<char> resultData;
+
+	auto retVal = ReadInput(resultData);
+	if (0 == retVal)
+	{
+		// do some program logic here...
+		cerr << "... doing program logic ..." << endl;
+		Sleep(5000);
+		
+		return WriteOurput(resultData);
+	}
+
+	return retVal;
 }
 
--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 # CapturedProcess
-C++ helper class to create a child process that has redirected std in/out/error streams via the Windows API.
+C++ helper class to create a child process with redirected std in/out/error streams 
+using the Windows API.
 
-It can be used to pass arbitrary binary input data to the child process via stdin and retrieve the result data via stdout. Errors can be received via stderr.
+It can be used to pass arbitrary binary input data to the child process via stdin and
+retrieve the result data via stdout. Errors can be received via stderr.
+
+## BSD License
+Feel free to use. See `LICENSE` file for further information.
